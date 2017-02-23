533b95b55a112933a2e73d1d9e7e88cd85f6c1cf
ArcQ <eddielaw296@gmail.com>
2017-02-23 01:27:17 -0500

    temp

--- src/components/redux-variable-form-fields.js
+++ src/components/redux-variable-form-fields.js
@@ -1,58 +1,19 @@
 import React, { PropTypes } from 'react';
-import update from 'immutability-helper';
-
-function linkIfVarInput(ele, row, inputState, createFieldInputHandler, fieldData) {
-  if (ele && ele.props) {
-    if (ele.props.varInput) {
-      const { varInput, ...restProps } = ele.props; // eslint-disable-line no-unused-vars
-      // if no field.key, we just wont' track it, no err req
-      const f = update(ele,
-        (ele.key)
-        ? {
-          props: {
-            $set: {
-              ...restProps,
-              key: ele.key + row,
-              value: (fieldData && fieldData.getIn([row, ele.key + row])) || '',
-              onChange: createFieldInputHandler(row, ele.key + row),
-            },
-          },
-        }
-        : {});
-      return f;
-    }
-    return update(ele, {
-      props: {
-        children: {
-          $set: React.Children.map(ele.props.children, child =>
-            linkIfVarInput(child, row, inputState, createFieldInputHandler, fieldData)),
-        },
-      },
-    });
-  }
-}
-
-const renderInputs = function (props) {
-  const {
-    inputList,
-    createFieldInputHandler,
-    children: fieldObj,
-    fieldData,
-  } = props;
-  return inputList.map((inputState, row) => {
-    if (fieldObj.constructor !== Array) {
-      return linkIfVarInput(fieldObj, row, inputState, createFieldInputHandler, fieldData);
-    }
-    return fieldObj.map(field =>
-      linkIfVarInput(field, row, inputState, createFieldInputHandler, fieldData));
-  });
-};
 
 export const ReduxVariableFormFields = props =>
-  <div> { renderInputs(props) } </div>;
+  <div>
+    {
+      React.Children.map(props.children, child =>
+        React.cloneElement(child, { formKey: props.formKey }))
+    }
+  </div>;
 
 ReduxVariableFormFields.propTypes = {
-  isCalcResult: PropTypes.bool.isRequired,
+  children: PropTypes.oneOf([
+    PropTypes.element,
+    PropTypes.arrayOf(PropTypes.element),
+  ]),
+  formKey: PropTypes.string,
 };
 
 export default ReduxVariableFormFields;
---
+++ src/components/variable-row.js
@@ -0,0 +1,70 @@
+import React, { PropTypes } from 'react';
+import update from 'immutability-helper';
+
+function makeVarInput(ele, row, createFieldInputHandler, fieldData) {
+  const { varInput, ...restProps } = ele.props; // eslint-disable-line no-unused-vars
+  // if no field.key, we just wont' track it, no err req
+  // should probably use React.cloneElement()
+  const input = update(ele,
+    (ele.key)
+    ? {
+      props: {
+        $set: {
+          ...restProps,
+          key: ele.key + row,
+          value: (fieldData && fieldData.getIn([row, ele.key + row])) || '',
+          onChange: createFieldInputHandler(row, ele.key + row),
+        },
+      },
+    }
+    : {});
+  return input;
+}
+
+function makeVarRemove(ele) {
+
+}
+
+function linkIfVarInput(ele, row, createFieldInputHandler, fieldData, inputState) {
+  if (ele && ele.props) {
+    if (ele.props.varInput) {
+      return makeVarInput(ele);
+    } else if (ele.props.varRemove) {
+      return makeVarRemove(ele);
+    }
+    return update(ele, {
+      props: {
+        children: {
+          $set: React.Children.map(ele.props.children, child =>
+            linkIfVarInput(child, row, createFieldInputHandler, fieldData, inputState)),
+        },
+      },
+    });
+  }
+  return null;
+}
+
+const renderInputs = function (props) {
+  const {
+    inputList,
+    createFieldInputHandler,
+    children: fieldObj,
+    fieldData,
+  } = props;
+  return inputList.map((inputState, row) => {
+    if (fieldObj.constructor !== Array) {
+      return linkIfVarInput(fieldObj, row, createFieldInputHandler, fieldData, inputState);
+    }
+    return fieldObj.map(field =>
+      linkIfVarInput(field, row, createFieldInputHandler, fieldData, inputState));
+  });
+};
+
+export const ReduxVariableFormFields = props =>
+  <div> { renderInputs(props) } </div>;
+
+ReduxVariableFormFields.propTypes = {
+  isCalcResult: PropTypes.bool.isRequired,
+};
+
+export default ReduxVariableFormFields;
